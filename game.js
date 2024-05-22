248a8e9e9ede5f374eede009d67b703d4e022a87
Miza <mizaeron@gmail.com>
2024-05-22 14:41:07 +0100

    add function to get user prompt

--- game.js
+++ game.js
@@ -9,3 +9,14 @@ function getComputerChoice(choices) {
 const options = ['Rock', 'Paper', 'Scissor'];
 const computerOption = getComputerChoice(options);
 console.log(computerOption);
+
+function getHumanChoice(message) {
+    
+    let userInput = prompt(message);
+
+    return userInput;
+}
+
+let name = getHumanChoice("Enter Rock Paper or Scissor:");
+
+console.log(name);
