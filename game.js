3c7bb1314ce5cb0d68393a1e7fe691edc12bf173
Miza <mizaeron@gmail.com>
2024-07-06 17:07:55 +0100

    Create conditional function where it links the generated random number to the string option

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-console.log("Hello World")
+function getComputerChoice() {
+
+    let option;
+
+    const randomIndex = Math.floor(Math.random() * 3);
+
+    if (randomIndex == 0) {
+        option = "rock";
+    } else if (randomIndex == 1) {
+        option = "paper";
+    } else {
+        option = "scissor";
+    }
+
+    return option;
+
+}
+
+const computerChoice = getComputerChoice();
+
+console.log(computerChoice);
+
